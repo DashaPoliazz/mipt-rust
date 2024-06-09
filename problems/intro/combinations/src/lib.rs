14121cdec96fc2a5c3eb635d0b3e0dfe2bfdaebf
DashaPoliazz <dasha.poliazzz@gmail.com>
2024-06-09 16:28:35 +0200

    'combinations' hw

--- problems/intro/combinations/src/lib.rs
+++ problems/intro/combinations/src/lib.rs
@@ -1,6 +1,18 @@
-#![forbid(unsafe_code)]
-
 pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    if k <= 0 {
+        return vec![vec![]];
+    }
+
+    let mut result = vec![];
+
+    for i in 0..arr.len() {
+        let remaining = &arr[i + 1..];
+        let sub_combination = combinations(remaining, k - 1);
+        for mut combination in sub_combination {
+            combination.insert(0, arr[i]);
+            result.push(combination);
+        }
+    }
+
+    return result;
 }
